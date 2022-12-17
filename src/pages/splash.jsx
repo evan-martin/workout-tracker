f9e92870bb44195ac1f3abc646c60a8b03b67179
evan-martin <evanm689@gmail.com>
2022-12-17 14:35:41 -0800

    add styles and logo to splash page

--- src/pages/splash.jsx
+++ src/pages/splash.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Logo from '../assets/logo.png'
+
+import './page-styles/splash.scss'
 
 const Splash = () => {
     const navigate = useNavigate();
 
     return (
         <div className='splash-container'>
+            <img src={Logo} alt='beep' className='logo' />
             <div className='title-container' >
                 <div className='title'>WorkIt</div>
-                <div className='sub-title'>A workout tracker</div>
+                <div className='sub-title'>A workout tracker and data visualizer</div>
+                <button className='outlined-button'>Enter</button>
             </div>
+
         </div>
     );
 }
