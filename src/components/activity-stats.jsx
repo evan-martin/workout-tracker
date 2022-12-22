52f48415c1f3a368aca81248516f91fe25de6aa4
evan-martin <evanm689@gmail.com>
2022-12-22 15:22:56 -0800

    don't render activity stat container if stat is not present

--- src/components/activity-stats.jsx
+++ src/components/activity-stats.jsx
@@ -26,94 +26,129 @@ const ActivityStats = ({ activity }) => {
 
     return (
         <div className='activity-stats-container'>
-            <div className='activity-stats-card'>
-                <div>
-                    <h2> {(activity.distance / 1609.34).toFixed(2)} mi</h2>
-                    <p>Distance</p>
+            {activity.distance !== 0 &&
+                <div className='activity-stats-card'>
+                    <div>
+                        <h2> {(activity.distance / 1609.34).toFixed(2)} mi</h2>
+                        <p>Distance</p>
+                    </div>
+                    <div className='icon'>
+                        <StraightenIcon fontSize='large' />
+                    </div>
                 </div>
-                <StraightenIcon fontSize='large' />
-            </div>
-            <div className='activity-stats-card'>
-                <div>
+            }
+            {activity.average_speed !== 0 &&
+                <div className='activity-stats-card'>
+                    <div>
+                        <h2>{(activity.average_speed * 2.237).toFixed(1)} mph</h2>
+                        <p>Average Speed</p>
+                    </div>
+                    <div className='icon'>
+                        <SpeedIcon fontSize='large' />
+                    </div>
+                </div>
+            }
+            {activity.max_speed !== 0 &&
+                <div className='activity-stats-card'>
+                    <div>
 
-                    <h2>{(activity.average_speed * 2.237).toFixed(1)} mph</h2>
-                    <p>Average Speed</p>
+                        <h2>{(activity.max_speed * 2.237).toFixed(1)} mph</h2>
+                        <p>Max Speed</p>
+                    </div>
+                    <div className='icon'>
+                        <SpeedIcon fontSize='large' />
+                    </div>
                 </div>
-                <SpeedIcon fontSize='large' />
-            </div>
-            <div className='activity-stats-card'>
-                <div>
-
-                    <h2>{(activity.max_speed * 2.237).toFixed(1)} mph</h2>
-                    <p>Max Speed</p>
+            }
+            {activity.moving_time !== 0 &&
+                <div className='activity-stats-card'>
+                    <div>
+                        <h2>{toTime(activity.moving_time)}</h2>
+                        <p>Moving Time</p>
+                    </div>
+                    <div className='icon'>
+                        <AccessTimeIcon fontSize='large' />
+                    </div>
                 </div>
-                <SpeedIcon fontSize='large' />
-            </div>
-            <div className='activity-stats-card'>
-                <div>
-
-                    <h2>{toTime(activity.moving_time)}</h2>
-                    <p>Moving Time</p>
+            }
+            {activity.elapsed_time !== 0 &&
+                <div className='activity-stats-card'>
+                    <div>
+                        <h2>{toTime(activity.elapsed_time)}</h2>
+                        <p>Total Time</p>
+                    </div>
+                    <div className='icon'>
+                        <AccessTimeIcon fontSize='large' />
+                    </div>
                 </div>
-                <AccessTimeIcon fontSize='large' />
-            </div>
-            <div className='activity-stats-card'>
-                <div>
-
-                    <h2>{toTime(activity.elapsed_time)}</h2>
-                    <p>Total Time</p>
+            }
+            {activity.total_elevation_gain !== 0 &&
+                <div className='activity-stats-card'>
+                    <div>
+                        <h2>{(activity.total_elevation_gain * 3.28).toFixed(0)} ft</h2>
+                        <p>Climb</p>
+                    </div>
+                    <div className='icon'>
+                        <TerrainIcon fontSize='large' />
+                    </div>
                 </div>
-                <AccessTimeIcon fontSize='large' />
-            </div>
-            <div className='activity-stats-card'>
-                <div>
-
-                    <h2>{(activity.total_elevation_gain * 3.28).toFixed(0)} ft</h2>
-                    <p>Climb</p>
+            }
+            {activity.average_temp !== undefined &&
+                <div className='activity-stats-card'>
+                    <div>
+                        <h2>
+                            {(activity.average_temp * (9 / 5) + 32).toFixed(0)} &#8457;</h2>
+                        <p>Average Temp</p>
+                    </div>
+                    <div className='icon'>
+                        <ThermostatIcon fontSize='large' />
+                    </div>
                 </div>
-                <TerrainIcon fontSize='large' />
-            </div>
-            <div className='activity-stats-card'>
-                <div>
-
-                    <h2>
-                    {(activity.average_temp*(9/5)+32).toFixed(0)} f</h2>
-                    <p>Average Temp</p>
+            }
+            {activity.average_watts !== undefined &&
+                <div className='activity-stats-card'>
+                    <div>
+                        <h2>{activity.average_watts} W</h2>
+                        <p>Average Power</p>
+                    </div>
+                    <div className='icon'>
+                        <BoltIcon fontSize='large' />
+                    </div>
                 </div>
-                <ThermostatIcon fontSize='large' />
-            </div>
-            <div className='activity-stats-card'>
-                <div>
-
-                    <h2>{activity.average_watts} W</h2>
-                    <p>Average Power</p>
+            }
+            {activity.kilojoules !== undefined &&
+                <div className='activity-stats-card'>
+                    <div>
+                        <h2>{activity.kilojoules} kj</h2>
+                        <p>Energy Output</p>
+                    </div>
+                    <div className='icon'>
+                        <BoltIcon fontSize='large' />
+                    </div>
                 </div>
-                <BoltIcon fontSize='large' />
-            </div>
-            <div className='activity-stats-card'>
-                <div>
-
-                    <h2>{activity.kilojoules} kj</h2>
-                    <p>Energy Output</p>
+            }
+            {activity.average_heartrate !== undefined &&
+                <div className='activity-stats-card'>
+                    <div>
+                        <h2>{(activity.average_heartrate).toFixed(0)} bpm</h2>
+                        <p>Average Heartrate</p>
+                    </div>
+                    <div className='icon'>
+                        <FavoriteIcon fontSize='large' />
+                    </div>
                 </div>
-                <BoltIcon fontSize='large' />
-            </div>
-            <div className='activity-stats-card'>
-                <div>
-
-                    <h2>{(activity.average_heartrate).toFixed(0)} bpm</h2>
-                    <p>Average Heartrate</p>
+            }
+            {activity.max_heartrate !== undefined &&
+                <div className='activity-stats-card'>
+                    <div>
+                        <h2>{(activity.max_heartrate).toFixed(0)} bpm</h2>
+                        <p>Max Heartrate</p>
+                    </div>
+                    <div className='icon'>
+                        <FavoriteIcon fontSize='large' />
+                    </div>
                 </div>
-                <FavoriteIcon fontSize='large' />
-            </div>
-            <div className='activity-stats-card'>
-                <div>
-
-                    <h2>{(activity.max_heartrate).toFixed(0)} bpm</h2>
-                    <p>Max Heartrate</p>
-                </div>
-                <FavoriteIcon fontSize='large' />
-            </div>
+            }
         </div>
     );
 
